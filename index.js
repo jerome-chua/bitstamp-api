6377300ffd6c36a5627e6c29df90232717721fbd
Jchu4 <jerome.chuame@gmail.com>
2021-07-29 11:52:39 +0800

    Add user balance object and install axios

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-const express = require('express');
+import axios from 'axios';
+import express from 'express';
 const app = express()
-const port = 3000
+const API = 'https://www.bitstamp.net/api/v2/ticker/';
+
+app.use(express.json());
+
+// Users' balances
+const userBalances = {
+  "user-1": {
+    "BTC": "0.5",
+    "ETH": "2"
+  },
+  "user-2": {
+    "BTC": "0.1",
+  },
+  "user-3": {
+    "ETH": "5",
+  },
+}
 
 app.get('/', (req, res) => {
   res.send('testing')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+const PORT = 3000
+app.listen(PORT, () => {
+  console.log(`App listening at http://localhost:${PORT}`)
 })
+
